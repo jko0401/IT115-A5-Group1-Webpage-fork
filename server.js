b39a3d4e9a4abe2dc86e0fb93de51b2021bba820
jko0401 <jko0401@gmail.com>
2020-12-07 20:50:34 -0800

    moved to public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static('/'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname });
